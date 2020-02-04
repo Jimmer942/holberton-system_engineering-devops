d973b78f8f778122ea1757e1642325fd94ef33f1
Jimmer942 <942@holbertonschool.com>
2020-02-03 19:04:01 -0500

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 """  Python script to export data in the CSV format """
 
+import csv
 import requests
 import sys
-import csv
+
 
 if __name__ == "__main__":
     r0 = requests.get('https://jsonplaceholder.typicode.com/todos',
