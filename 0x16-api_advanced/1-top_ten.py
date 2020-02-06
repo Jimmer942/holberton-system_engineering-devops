1a3973b024ed401207475f07f6da07d371570694
Jimmer942 <942@holbertonschool.com>
2020-02-06 18:00:59 -0500

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,9 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/.json'.format(subreddit)
     req = requests.get(url, headers=headers, params=params)
     if req.status_code != 200:
-        return 0
-    req = req.json()
-    hots = req['data']['children']
-    if len(hots) is 0:
         print(None)
         return
+    req = req.json()
+    hots = req['data']['children']
     for i in hots:
         print(i['data']['title'])
