feda08660e4fd83f11c379e79b0967401634a6a8
Jimmer942 <942@holbertonschool.com>
2020-02-19 21:49:32 -0500

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-#Web debbuging
+# Web debbuging
 exec { 'Correct file and restart':
   command  => 'sudo sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php && sudo service apache2 restart',
-    provider => shell,
+  provider => shell,
 }
